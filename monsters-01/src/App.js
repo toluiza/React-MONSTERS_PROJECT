c889e875cef7f0e9ef5b6370821b2fd1cb38b6cb
Luiza <45511173+toluiza@users.noreply.github.com>
2023-01-03 15:30:07 +0000

    Add state

--- monsters-01/src/App.js
+++ monsters-01/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      name: {firstName: 'Luiza', lastName: 'Jurczyk'},
+      age: '34'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hi {this.state.name.firstName} {this.state.name.lastName}</h1>
+          <p>I and {this.state.age} years old.</p>
+          <button onClick={() => {
+            this.setState({name: {firstName: "Iza", lastName: "Tadam"}})
+          }}>Change name</button>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
