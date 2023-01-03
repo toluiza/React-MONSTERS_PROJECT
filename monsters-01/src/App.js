26debe9425d82ff2edda4a13d0d57c5a31a19a90
luiza <toluiza@gmail.com>
2023-01-03 20:06:04 +0100

    Use setState

--- monsters-01/src/App.js
+++ monsters-01/src/App.js
@@ -1,29 +1,43 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 class App extends Component {
+	constructor() {
+		super();
 
-  constructor(){
-    super();
+		this.state = {
+			name: { firstName: "Luiza", lastName: "Jurczyk" },
+			age: "34",
+		};
+	}
 
-    this.state = {
-      name: {firstName: 'Luiza', lastName: 'Jurczyk'},
-      age: '34'
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hi {this.state.name.firstName} {this.state.name.lastName}</h1>
-          <p>I and {this.state.age} years old.</p>
-          <button onClick={() => {
-            this.setState({name: {firstName: "Iza", lastName: "Tadam"}})
-          }}>Change name</button>
-        </header>
-      </div>
-    );
-  };
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>
+						Hi {this.state.name.firstName} {this.state.name.lastName}
+					</h1>
+					<p>I and {this.state.age} years old.</p>
+					<button
+						onClick={() =>
+							this.setState(
+								() => {
+									return {
+										name: { firstName: "Iza", lastName: "Tadam" },
+									};
+								},
+								() => {
+									console.log(this.state);
+								}
+							)
+						}
+					>
+						Change name
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
