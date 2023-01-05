b26610b84774859d31fcb131c34f57ecb2c1f11f
luiza <toluiza@gmail.com>
2023-01-05 21:10:41 +0100

    Refactor the code with CardList component

--- monsters-01/src/App.js
+++ monsters-01/src/App.js
@@ -1,30 +1,53 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			monsters: [
-				{
-					name: "Linda",
-				},
-				{
-					name: "Frank",
-				},
-				{
-					name: "Jackie",
-				},
-			],
+			monsters: [],
+			searchField: "",
 		};
 	}
 
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(() => {
+					return { monsters: users };
+				})
+			);
+	}
+
+	onSearchChange = (event) => {
+		console.log(event.target.value);
+		const searchField = event.target.value.toLowerCase();
+
+		this.setState(() => {
+			return { searchField };
+		});
+	};
+
 	render() {
+		console.log("render");
+
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+		const filteredMonsters = this.state.monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(searchField);
+		});
+
 		return (
 			<div className="App">
-				{this.state.monsters.map((monster) => {
-					return <h1>{monster.name}</h1>;
-				})}
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={onSearchChange}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
